d73610dd3a7b47b8bc52569d23c3936acddeab10
Amir Ramzan <amir.ramzan@7vals.com>
2016-02-19 11:42:55 +0500

    fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,8 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
   
-  protect_from_forgery with: :exception
-  rescue_from Exception, with: :handle_exceptions
+  protect_from_forgery    with: :exception
+  rescue_from Exception,  with: :handle_exceptions
    
   def current_ability
     @current_ability ||= AbilityFactory.build_ability_for(current_user)
@@ -14,14 +14,23 @@ class ApplicationController < ActionController::Base
   # ------------ Exceptions -----------
   private
   def handle_exceptions(exception)
-    respond_to do |format|
-      format.html do
-        flash[:error] = exception.message
-        redirect_to root_path 
-      end
-      format.js do
-        flash.now[:error] = exception.message
-        render "/shared/errors.js.erb" 
+    logger.error "Error Message: #{exception.message}"
+    logger.error exception.backtrace.join("\n")
+#    exception.backtrace.each { |line| logger.error line }
+    
+    case exception
+    when ActionController::RoutingError
+      not_found
+    else
+      respond_to do |format|
+        format.html do
+          flash[:error] = exception.message
+          redirect_to root_path 
+        end
+        format.js do
+          flash.now[:error] = exception.message
+          render "/shared/errors.js.erb" 
+        end
       end
     end
   end
@@ -30,5 +39,9 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.for(:sign_up) do |u|
       u.permit(:name, :email, :password, :password_confirmation)
     end
+  end
+  
+  def not_found
+    render file: "#{Rails.root}/public/404.html", layout: false, status: 404
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def page_title(sub_title = '')
     base_title = t(:app_title)
 
-    if sub_title.empty?
+    if sub_title.blank?
       base_title
     else
       base_title + " - " + sub_title
